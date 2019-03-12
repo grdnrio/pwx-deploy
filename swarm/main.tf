28a5a8af434f8b5763ce4d4934fc9e1abd4b1142
Joe Gardiner <joe@Joes-iMac.local>
2019-03-12 13:28:09 +0000

    Switched to networking module

--- swarm/main.tf
+++ swarm/main.tf
@@ -30,69 +30,8 @@ data "aws_ami" "default" {
     owners = ["099720109477"] # Canonical
 }
 
-# Create a VPC to launch our instances into
-resource "aws_vpc" "default" {
-  cidr_block = "10.0.0.0/16"
-}
-
-# Create an internet gateway to give our subnet access to the outside world
-resource "aws_internet_gateway" "default" {
-  vpc_id = "${aws_vpc.default.id}"
-}
-
-# Grant the VPC internet access on its main route table
-resource "aws_route" "internet_access" {
-  route_table_id         = "${aws_vpc.default.main_route_table_id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.default.id}"
-}
-
-# Create a subnet to launch our instances into
-resource "aws_subnet" "default" {
-  vpc_id                  = "${aws_vpc.default.id}"
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-}
-
-# Our default security group to access
-# the instances over SSH and HTTP
-resource "aws_security_group" "default" {
-  name        = "terraform_sg_default"
-  description = "Used in the terraform"
-  vpc_id      = "${aws_vpc.default.id}"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 30000
-    to_port     = 35000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    self = true
-  }
-
-  # outbound internet access
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+module "aws_networking" {
+  source = "../modules/network"
 }
 
 resource "aws_instance" "master" {
@@ -102,25 +41,16 @@ resource "aws_instance" "master" {
   }
 
   connection {
-    # The default username for our AMI
     user = "ubuntu"
     private_key = "${file(var.private_key_path)}"
   }
-
   associate_public_ip_address = true
   private_ip = "10.0.1.10"
   instance_type = "t2.medium"
-  # Lookup the correct AMI based on the region
-  # we specified
   ami = "${data.aws_ami.default.id}"
-
-  # The name of our SSH keypair we created above.
   key_name = "${var.key_name}"
-
-  # Our Security group to allow HTTP and SSH access
-  vpc_security_group_ids = ["${aws_security_group.default.id}"]
-  subnet_id = "${aws_subnet.default.id}"
-
+  vpc_security_group_ids = ["${module.aws_networking.security_group}"]
+  subnet_id = "${module.aws_networking.subnet}"
   root_block_device = {
     volume_type = "gp2"
     volume_size = "20"
@@ -170,8 +100,8 @@ resource "aws_instance" "worker" {
   private_ip = "10.0.1.1${count.index +1}"
   ami = "${data.aws_ami.default.id}"
   key_name = "${var.key_name}"
-  vpc_security_group_ids = ["${aws_security_group.default.id}"]
-  subnet_id = "${aws_subnet.default.id}"
+  vpc_security_group_ids = ["${module.aws_networking.security_group}"]
+  subnet_id = "${module.aws_networking.subnet}"
   root_block_device = {
     volume_type = "gp2"
     volume_size = "20"
