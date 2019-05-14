e866882bdce38c3b872ccaefb3b14232afc0f808
Joe Gardiner <joe@grdnr.io>
2019-05-14 18:11:48 +0100

    initialisation

---
+++ kubernetes/px-metro/variables.tf
@@ -0,0 +1,24 @@
+#############################################################
+##
+## This app file contains the variables of Kubernetes Installation
+## on AWS
+## 
+## @year 2019
+## @author Joe Gardiner <joe@grdnr.io>
+##
+#############################################################
+### Region
+variable "aws_region" {
+  description = "The region to deploy the kubernetes clusters"
+  default=""
+}
+
+variable "key_name" {
+  description = "Key pair name to use for SSH"
+  default = ""
+}
+
+variable "private_key_path" {
+  description = "Path to private SSH key"
+  default = ""
+}
